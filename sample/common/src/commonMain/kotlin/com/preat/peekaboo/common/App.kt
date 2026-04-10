9563362a96d854ddca41177058e862fefab9ff17
Mazhar Hussain <mazhar.hussain@texense.io>
2026-04-10 16:40:48 +0500

    Fix sample compilation

--- sample/common/src/commonMain/kotlin/com/preat/peekaboo/common/App.kt
+++ sample/common/src/commonMain/kotlin/com/preat/peekaboo/common/App.kt
@@ -15,6 +15,7 @@
  */
 package com.preat.peekaboo.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -22,6 +23,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -41,6 +43,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.preat.peekaboo.common.component.PeekabooCameraView
 import com.preat.peekaboo.common.component.PeekabooGalleryView
